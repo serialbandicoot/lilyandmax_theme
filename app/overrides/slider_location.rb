5aa6d86bf25e96f057b1dac2352080154feed438
Sam <samtreweek@gmail.com>
2012-05-01 10:22:25 +0100

    Only one override now in the theme, most of the work is in the spree_slider gem

--- app/overrides/slider_location.rb
+++ app/overrides/slider_location.rb
@@ -2,12 +2,12 @@ Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
                      :name => 'add_slider',
                      :remove => '#spree-slider')
 
-Deface::Override.new(:virtual_path => "spree/home/index",
-                     :name => "lam_add_slider",
-                     :insert_after => "#news_ticker",
-                     :text => %q{
-                     				    <div id='spree-slider' data-hook>
-                                      <p></p>
-                                  		<%= yield :spree_slider %>
-                                	</div>
-                                })
+#Deface::Override.new(:virtual_path => "spree/home/index",
+#                     :name => "lam_add_slider",
+#                     :insert_after => "#news_ticker",
+#                     :text => %q{
+#                     				    <div id='spree-slider' data-hook>
+#                                      <p></p>
+#                                  		<%= yield :spree_slider %>
+#                                	</div>
+#                                })
