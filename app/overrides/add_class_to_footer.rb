95d40613d47b096420989ef6976d442ef12ed5fb
Sam <samtreweek@gmail.com>
2012-05-03 11:40:55 +0100

    Added Social to footer, which currently includes twitter

--- app/overrides/add_class_to_footer.rb
+++ app/overrides/add_class_to_footer.rb
@@ -9,7 +9,7 @@ Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
                      :insert_before => '#footer-left',
                      :text => %q{
                         <div data-hook="footer_columns">
-                            <div class="columns_three">
+                            <div class="columns_customer_service">
                                <h3><span>Customer Service</span></h3>
                                 <li><%= link_to "Delivery Information", delivery_path %></li>
                                 <li><%= link_to "FAQ's", faq_path %></li>
@@ -24,7 +24,7 @@ Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
                                 <li><%= link_to "Newsletter", news_letter_path %></li>
                                 <li><%= link_to "Contact us", contact_us_path %></li>
                             </div>
-                            <div class="columns_four">
+                            <div class="columns_payment_options">
                               <h3><span>Payment options</span></h3>
                                 <!-- Moneybookers -->
                                 <a href='https://www.moneybookers.com/'>
@@ -33,7 +33,7 @@ Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
                                 <!-- PayPal Logo -->
                                 <a href="#" onclick="javascript:window.open('https://www.paypal.com/uk/cgi-bin/webscr?cmd=xpt/Marketing/popup/OLCWhatIsPayPal-outside','olcwhatispaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=500');"><img  src="https://www.paypal.com/en_GB/Marketing/i/logo/PayPal_logo_80x35.gif" border="0" alt="PayPal-Standard-Logo"></a><!-- PayPal Logo -->
                             </div>
-                            <div class="columns_five">
+                            <div class="columns_security">
                               <h3><span> Security</span></h3>
                               <div class='comodo'>
                                 <a href="http://www.comodo.com/e-commerce/ssl-certificates/secure-server.php">
@@ -42,10 +42,17 @@ Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
                                 <script language="JavaScript" src="http://www.comodo.com/javascript/banner.js" type="text/javascript"></script>
                               </div>
                             </div>
-                            <div class="columns_six">
+                            <div class="columns_newsletter">
                               <h3><span> Newsletter</span></h3>
-                                <div id="location_for_newsletter"></div>
-                              </div>
+                                <div id="location_for_newsletter">
+                                </div>
+                            </div>
+                            <div class="columns_social_media">
+                              <h3><span> Social</span></h3>
+                                <div id="social_media_links">
+                                  <a href="https://twitter.com/lilyandmax_uk" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow @lilyandmax_uk</a>
+                                  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                                </div>
                             </div>
 
                         </div>
