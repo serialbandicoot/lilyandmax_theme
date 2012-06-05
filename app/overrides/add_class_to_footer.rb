c5be20f262d4b1ea454c6d876566af6a8bdf4a66
Sam <samtreweek@gmail.com>
2012-06-05 08:19:29 +0100

    Added facebook main business like to homepage

--- app/overrides/add_class_to_footer.rb
+++ app/overrides/add_class_to_footer.rb
@@ -54,6 +54,15 @@ Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
                                   <a href="https://twitter.com/lilyandmax_uk" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow @lilyandmax_uk</a>
                                   <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
                                 </div>
+                                <div id="fb-root"></div>
+                                  <script>(function(d, s, id) {
+                                    var js, fjs = d.getElementsByTagName(s)[0];
+                                    if (d.getElementById(id)) return;
+                                    js = d.createElement(s); js.id = id;
+                                    js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+                                    fjs.parentNode.insertBefore(js, fjs);
+                                  }(document, 'script', 'facebook-jssdk'));</script>
+                                  <div class="fb-like" data-href="http://www.facebook.com/lilyandmaxparties" data-send="true" data-layout="button_count" data-width="250" data-show-faces="true"></div>
                             </div>
 
                         </div>
